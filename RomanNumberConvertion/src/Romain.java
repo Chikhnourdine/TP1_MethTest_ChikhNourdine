70781313c811d46fe48c6b4d1328d343c1b85756
Chikh Nourdine <chikhnourdine@gmail.com>
2013-10-27 16:12:32 +0100

    Factorisation du test sur les chiffres romains

--- RomanNumberConvertion/src/Romain.java
+++ RomanNumberConvertion/src/Romain.java
@@ -1,11 +1,25 @@
 
 public class Romain {
 
-	public Romain(String string) {
+	private final String romain;
+	public Romain(String romain) {
+		this.romain = romain;
 		
 	}
-
 	public int ToEntier() {
+		if (romain.equals("M"))
+			return 1000;
+		if (romain.equals("D"))
+			return 500;
+		if (romain.equals("C"))
+			return 100;
+		if (romain.equals("L"))
+			return 50;
+		if (romain.equals("X"))
+			return 10;
+		if (romain.equals("V"))
+			return 5;
+		
 		return 1;
 	}
 
--- RomanNumberConvertion/src/TestRomanToInt.java
+++ RomanNumberConvertion/src/TestRomanToInt.java
@@ -6,8 +6,30 @@ import junit.framework.Test;
 public class TestRomanToInt {
 	@org.junit.Test
 	public void I_convet_to_1() throws Exception {
-		Romain romain = new Romain("I");
-		int entier = romain.ToEntier();
-		assertEquals(1, entier);		
+		assertEquals(1, new Romain("I").ToEntier());		
+	}
+	@org.junit.Test
+	public void V_convet_to_5() throws Exception {
+		assertEquals(5, new Romain("V").ToEntier());		
+	}
+	@org.junit.Test
+	public void X_convet_to_10() throws Exception {
+		assertEquals(10, new Romain("X").ToEntier());		
+	}
+	@org.junit.Test
+	public void L_convet_to_50() throws Exception {
+		assertEquals(50, new Romain("L").ToEntier());		
+	}
+	@org.junit.Test
+	public void C_convet_to_100() throws Exception {
+		assertEquals(100, new Romain("C").ToEntier());		
+	}
+	@org.junit.Test
+	public void D_convet_to_500() throws Exception {
+		assertEquals(500, new Romain("D").ToEntier());		
+	}
+	@org.junit.Test
+	public void M_convet_to_1000() throws Exception {
+		assertEquals(1000, new Romain("M").ToEntier());		
 	}
 }
